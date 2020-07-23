43d9cc90e2193e5e54b8d93c7845ff7a0ee28443
xingyun86 <xingyun_ppshuai@yeah.net>
2020-07-23 14:30:53 +0800

    Add codes

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # StreamMediumServer
-StreamMediumServer, a cross platform stream medium server.
+StreamMediumServer, a cross platform stream medium server.base on live five five five, support cmake build.
---
+++ src/StreamMediumServer/StreamMediumServer/StreamMediumServer.cpp
@@ -0,0 +1,12 @@
+﻿// StreamMediumServer.cpp : Defines the entry point for the application.
+//
+
+#include "StreamMediumServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
